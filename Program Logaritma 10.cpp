2cd2862822ba7f6186ab9a1e0920bc2ce81f7c82
Risky David K <89583633+riskydavid25@users.noreply.github.com>
2021-09-14 16:04:06 +0700

    Create Program Logaritma 10.cpp

---
+++ Program Logaritma 10.cpp
@@ -0,0 +1,15 @@
+//Program Bilangan Logaritma 10
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float x,hasil;
+    cout << "bilangan = ";
+    cin >> x;
+    hasil=log10(x);
+    cout << "hasil logaritma= " << hasil << endl;
+    return 0;
+}
