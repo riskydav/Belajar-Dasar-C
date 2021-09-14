5344334277598ac3b886a39f5aeb52c3db3bc39d
Risky David K <89583633+riskydavid25@users.noreply.github.com>
2021-09-14 15:21:03 +0700

    Create Program Pemilihan Tunggal.cpp

---
+++ Program Pemilihan Tunggal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int usia;
+    cin >> usia;
+    if (usia < 17)
+    {
+        cout << "Belum cukup umur, dilarang menonton!!!" << endl;
+    }
+    cout << "Terima kasih" << endl;
+    
+    return 0;
+}
